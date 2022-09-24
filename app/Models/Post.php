594f32f565bd0b75faaf61da188d5a5e6cf47f98
tetuyakawarada <tetuya.kawarada@gmail.com>
2022-09-24 15:59:13 +0900

    エラーコードの共通化

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+    ];
 
     /**
      * Get the user that owns the Post
@@ -17,5 +23,10 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::url('images/posts/' . $this->image);
     }
 }
