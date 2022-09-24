670e7bebab7e42d7ff74e44c84e5c7fedd83b11b
tetuyakawarada <tetuya.kawarada@gmail.com>
2022-09-24 17:44:35 +0900

    削除処理作成

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -27,6 +27,12 @@ class Post extends Model
 
     public function getImageUrlAttribute()
     {
-        return Storage::url('images/posts/' . $this->image);
+        return Storage::url($this->image_path);
     }
+
+    public function getImagePathAttribute()
+    {
+        return 'images/posts/' . $this->image;
+    }
+
 }
